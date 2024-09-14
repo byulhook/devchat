f506f419aa41ef679e66e519c3e53dcb2cdf933e
nakyeonko3 <badaclock@gmail.com>
2024-09-14 16:45:01 +0900

    chore: 프로젝트 초기 세팅(#1)
    - eslint, prettier 코드 포맷팅 세팅
    - byul, byulhook 깃훅 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,74 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'import', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: ['^/'],
+      },
+    ],
+  },
+  ignorePatterns: ['node_modules/', 'build/', 'dist/', '.github', '.tmp'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+      node: {
+        extensions: ['.js', '.ts', '.tsx'],
+        paths: ['src'],
+      },
+      alias: {
+        map: [
+          ['@', './src'],
+          ['/', './public'],
+        ],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.svg'],
+      },
+    },
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    visualizer(),
+    tsconfigPaths(),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+});

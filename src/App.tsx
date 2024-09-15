270854636b17b3ffa29b6ff8e2c37ba2406258e1
nakyeonko3 <badaclock@gmail.com>
2024-09-15 14:03:10 +0900

    feat: 리액트 라우터돔 설치 및 네비바 설정중 (#3)
    - chore: react-router-dom, @types/react-router-dom 종속성 설치
    - remove: App.css, index.css 삭제
    - feat: routes/path.ts에 경로들을 저장하는 PATH 변수를 생성함.
    - 네비게이션바가 있는 레이아웃과 네비게이션바가 없는 레이아웃 페이지를
      만들 예정
    - 로그인 페이지는 네비게이션바가 없어야함
    - 네비게이션바는 사이드 왼쪽에 위치해 있어야함

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,111 @@
-import { useState } from 'react';
+import { css, SerializedStyles } from '@emotion/react';
+import { createBrowserRouter, Outlet, RouterProvider, Link } from 'react-router-dom';
 
-import './App.css';
-import { css } from '@emotion/react';
-
+import { PATH } from '@/routes/path';
 import theme from '@/styles/theme';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+interface ContainerLayoutProps {
+  customStyle?: SerializedStyles;
+}
+// 루트 레이아웃: 네비바가 없는 레이아웃 컴포넌트
+const RootLayout: React.FC<ContainerLayoutProps> = ({ customStyle }) => {
   return (
     <>
-      <div css={styleV1}>
-        <h1>hello</h1>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <main css={[mainStyle, customStyle]}>
+        <Outlet />
+      </main>
     </>
   );
-}
+};
 
-const styleV1 = css`
-  color: ${theme.colors.primary};
+export const mainStyle = css`
+  width: 100%;
+  max-width: ${theme.width.max};
+  height: 100%;
+  min-height: 100vh;
+  padding: 76px 16px 0;
+  margin: 0 auto;
+  background-color: ${theme.colors.white};
+
+  @media screen and (min-width: ${theme.width.max}) {
+    border-left: 1px solid ${theme.colors.lightGray};
+    border-right: 1px solid ${theme.colors.lightGray};
+  }
 `;
+// 네비게이션바
+// 하단이 아닌 왼쪽 사이드에 위치해 있어야함.
+const Navbar = () => <div>네비게이션바</div>;
+
+// 컨테이너 레이아웃: 네비바가 있는 레이아웃 컴포넌트
+const ContainerLayout = () => {
+  return (
+    <>
+      <RootLayout
+        customStyle={css`
+          padding-bottom: 86px;
+          min-height: calc(100vh - 86px);
+        `}
+      />
+      <Navbar />
+    </>
+  );
+};
+
+// NotFoundPage
+const NotFoundPage = () => (
+  <>
+    <div>페이지를 찾을수 없습니다.</div>
+    <Link to={PATH.HOME}>
+      <h5>홈으로 이동</h5>
+    </Link>
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: PATH.SIGN_IN,
+        element: <div>로그인</div>,
+      },
+    ],
+  },
+  {
+    path: PATH.HOME,
+    element: <ContainerLayout />,
+    errorElement: <div>Error Page</div>,
+    children: [
+      {
+        path: PATH.MESSAGING,
+        element: <div>메시지 작성</div>,
+      },
+      {
+        path: PATH.FRIENDS,
+        element: <div>친구 목록</div>,
+      },
+      {
+        path: PATH.PROFILE,
+        element: <div>프로필 페이지</div>,
+      },
+      {
+        path: PATH.SETTINGS,
+        element: <div>앱 설정 페이지</div>,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <ContainerLayout />,
+    children: [
+      {
+        path: '*',
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
---
+++ src/routes/path.ts
@@ -0,0 +1,9 @@
+export const PATH = {
+  HOME: '/',
+  SIGN_IN: '/signin',
+  MESSAGING: '/messaging',
+  FRIENDS: '/friends',
+  SETTINGS: '/settings',
+  PROFILE: '/profile',
+  SEARCH: '/search',
+} as const;

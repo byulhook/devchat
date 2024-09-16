f97bfe323533cecca5ec9b2dc1f4d0f19c3b8f3a
nakyeonko3 <badaclock@gmail.com>
2024-09-16 21:38:25 +0900

    design: 로그인 페이지 디자인 수정 (#7)
    - 데모 버튼 삭제
    - 로그인 페이지 이미지 변경

--- src/pages/Siginin.tsx
+++ src/pages/Siginin.tsx
@@ -1,8 +1,7 @@
 import { css } from '@emotion/react';
-import { Link } from 'react-router-dom';
 
 import githubLogo from '@/assets/github.png';
-import loginBackground from '@/assets/login-background.png';
+import loginBackground from '@/assets/logo.png';
 import Button from '@/components/Button';
 import theme from '@/styles/theme';
 
@@ -19,17 +18,6 @@ const Siginin = () => {
         </div>
         <div css={loginButtonContiner}>
           <Button image={githubLogo} text="Log in with Github!" customStyle={githubButtonStyle} />
-          <div css={dividerStyle}>
-            <hr />
-            <span>or</span>
-            <hr />
-          </div>
-          <Button text="Try Demo Mode!" onClick={() => {}} customStyle={demoButtonStyle} />
-        </div>
-        <div css={additionalLinksContainer}>
-          <Link to="#">Learn more about DevChat</Link>
-          <Link to="#">Terms of Service</Link>
-          <Link to="#">Privacy Policy</Link>
         </div>
       </section>
     </div>
@@ -45,12 +33,15 @@ const siginPageWrapper = css`
 `;
 
 const imageContainer = css`
-  width: 50%;
+  width: 60%;
   height: 100%;
   overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   img {
-    width: 100%;
-    height: 100%;
+    width: 665px;
+    height: 665px;
     object-fit: cover;
   }
 `;
@@ -58,9 +49,9 @@ const imageContainer = css`
 const signinPageContainer = css`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   background-color: ${theme.colors.darkestBlack};
-  width: 50%;
+  width: 40%;
   padding: 48px;
 `;
 
@@ -86,6 +77,7 @@ const loginButtonContiner = css`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-top: 100px;
   width: 100%;
 `;
 
@@ -98,55 +90,5 @@ const githubButtonStyle = css`
 
   &:hover {
     background-color: ${theme.colors.gray300};
-  }
-`;
-
-const demoButtonStyle = css`
-  background-color: ${theme.colors.primary};
-  color: ${theme.colors.white};
-  font-weight: 600;
-  border-radius: 24px;
-  transition: all 0.3s ease;
-
-  &:hover {
-    background-color: ${theme.colors.black};
-  }
-`;
-
-const dividerStyle = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  max-width: 340px;
-  margin: 24px 0;
-
-  hr {
-    flex-grow: 1;
-    border: none;
-    border-top: 1px solid ${theme.colors.gray700};
-    margin: 0 16px;
-  }
-
-  span {
-    font-weight: 400;
-    font-size: 14px;
-    color: ${theme.colors.gray300};
-  }
-`;
-
-const additionalLinksContainer = css`
-  display: flex;
-  justify-content: center;
-  gap: 24px;
-
-  a {
-    color: ${theme.colors.gray400};
-    font-size: ${theme.fontSizes.small};
-    transition: color 0.3s ease;
-
-    &:hover {
-      color: ${theme.colors.gray100};
-    }
   }
 `;
